289563881f6886daac07dca845599de58eed99ff
Noelia <perezgonzaleznoelia08@gmail.com>
2022-11-09 13:45:17 +0100

    tareilla

---
+++ src/main/java/morra/EjercicioRepaso.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package morra;
+
+/**
+ *
+ * @author noelia
+ */
+public class EjercicioRepaso {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //los clientes de una tienda casi siempre tienen descuento al llegar a la 
+        //caja durante la semana loca. cuando van a pagar tienen que sacar
+        //una bola de una bolsa, que es la que nos indica el descuento.
+        //si la bola es color azul se obtiene un 20%, si es roja un 25%, 
+        //si es blanca no hay descuento, y si es verde se saca de la bolsa y se
+        //vuelva a sacar otra de las restantes.
+        //Escribe un programa que lea el importe a pagar por el cliente 
+        //e imprima la bola que "sacó" de la bolsa y el importe con el descuento 
+        //aplicado. El programa, una vez finaliza un cliente, pregunta si hay otro
+        //en la cola para operar de la misma forma. Si no hay clientes
+        //el programa termina.
+        //scanner, excepciones
+        
+        
+    }
+    
+}
